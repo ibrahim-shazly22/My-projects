eb140f8ddbe92302d0c3e0627b577169dd42f31a
ibrahim-shazly22 <ibrahimshazly2001@gmail.com>
2024-10-10 03:28:27 +0300

    Add files via upload

---
+++ project 26(Nato alphapet using pandas)/Draft.py
@@ -0,0 +1,18 @@
+list_comperehension=2
+# numbers=[1,2,3]
+# new_list=[num+1 for num in numbers]
+#
+# name="ibrahim"
+# new_lis=[letter for letter in name]
+#
+# my_list=[num*2 for num in range(1,5)]
+
+Dicctionray_comperehension=2
+# import random
+#
+# names=["ali","omar","mohamed","ibrahim","mido"]
+# students_results={student:random.randint(1, 50) for student in names}
+#
+# passed_students={student:score for(student,score) in students_results.items() if score>=10 }
+# print(passed_students)
+
